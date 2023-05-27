61f48c7e737a0a0d98c8fd829d66b79ce09d2ce5
AngelV02 <125764846+AngelV02@users.noreply.github.com>
2023-05-27 12:49:01 -0700

    Add project files.

---
+++ Employee.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Operators_Assign
+{
+    public class Employee
+    {
+        // Employee properties
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        // Overloading the "==" operator to compare Employee objects based on their Id
+        public static bool operator ==(Employee emp1, Employee emp2)
+        {
+            if (ReferenceEquals(emp1, emp2))
+                return true;
+
+            if (emp1 is null || emp2 is null)
+                return false;
+
+            return emp1.Id == emp2.Id;
+        }
+
+        // Overloading the "!=" operator to compare Employee objects based on their Id
+        public static bool operator !=(Employee emp1, Employee emp2)
+        {
+            return !(emp1 == emp2);
+        }
+    }
+}
